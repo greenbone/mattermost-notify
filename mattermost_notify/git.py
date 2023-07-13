a23a19b5fbc7cc826f6332eb849f47ae2f7fad3c
Björn Ricks <bjoern.ricks@greenbone.net>
2023-07-13 15:15:26 +0200

    Change: Refactor fill_template to expect arguments instead of a namespace
    The arguments should be declared on the function and not implicitly via
    a fitting namespace instance. This allows to what's actually required to
    get the function working as expected.

--- mattermost_notify/git.py
+++ mattermost_notify/git.py
@@ -37,7 +37,7 @@ SHORT_TEMPLATE = "{status}: {workflow} ({commit})  in {repository} ({branch})"
 DEFAULT_GIT = "https://github.com"
 
 
-def _linker(name: str, url: Optional[str] = None) -> str:
+def linker(name: str, url: Optional[str] = None) -> str:
     # create a markdown link
     return f"[{name}]({url})" if url else name
 
@@ -57,54 +57,73 @@ def get_github_event_json(term: ConsoleTerminal) -> dict[str, Any]:
         term.error("Could not find GitHub Event JSON file.")
     except json.JSONDecodeError:
         term.error("Could not decode the JSON object.")
+
     return {}
 
 
-def fill_template(args: Namespace, term: ConsoleTerminal) -> str:
+def fill_template(
+    *,
+    short: bool = False,
+    highlight: Optional[list[str]] = None,
+    commit: Optional[str] = None,
+    branch: Optional[str] = None,
+    repository: Optional[str] = None,
+    status: Optional[str] = None,
+    workflow_id: Optional[str] = None,
+    workflow_name: Optional[str] = None,
+    terminal: ConsoleTerminal,
+) -> str:
     template = LONG_TEMPLATE
-    if args.short:
+    if short:
         template = SHORT_TEMPLATE
 
     # try to get information from the GiTHUB_EVENT json
-    event = get_github_event_json(term)
-    if not event:
-        git_url = f"{DEFAULT_GIT}/{args.repository}"
-        status = Status[args.status.upper()].value
-        workflow = _linker(
-            args.workflow_name, f"{git_url}/actions/runs/{args.workflow}"
-        )
-        repository = _linker(args.repository, git_url)
-        branch = args.branch
-        commit = "not available"
-    else:
-        workflow_info = event["workflow_run"]
-        workflow = _linker(workflow_info["name"], workflow_info["html_url"])
-        head_commit = workflow_info["head_commit"]
-        head_repo = workflow_info["head_repository"]
-        repo_url = head_repo["html_url"]
-        repository = _linker(head_repo["full_name"], repo_url)
-        branch = _linker(
-            workflow_info["head_branch"],
-            f'{repo_url}/tree/{workflow_info["head_branch"]}',
-        )
-        commit_name: str = head_commit["message"].split("\n", 1)[0]
-        commit = _linker(commit_name, f'{repo_url}/commit/{head_commit["id"]}')
-        if workflow_info["conclusion"]:
-            status = Status[workflow_info["conclusion"].upper()].value
-        else:
-            status = Status.UNKNOWN.value
+    event = get_github_event_json(terminal)
+    workflow_info: dict[str, Any] = event.get("workflow_run", {})
 
-    highlight = ""
-    if args.highlight and status is not Status.SUCCESS.value:
-        highlight = "".join([f"@{h}\n" for h in args.highlight])
+    status = status if status else workflow_info.get("conclusion")
+    workflow_status = Status[status.upper()] if status else Status.UNKNOWN
+
+    workflow_name: str = (
+        workflow_name if workflow_name else workflow_info.get("name", "")
+    )
+
+    head_repo: dict[str, Any] = workflow_info.get("head_repository", {})
+    repository = repository if repository else head_repo.get("full_name", "")
+    repository_url = (
+        f"{DEFAULT_GIT}/{repository}"
+        if repository
+        else head_repo.get("html_url", "")
+    )
+
+    branch: str = branch if branch else workflow_info.get("head_branch", "")
+    branch_url = f"{repository_url}/tree/{branch}"
+
+    workflow_url = (
+        f"{repository}/actions/runs/{workflow_id}"
+        if repository
+        else workflow_info.get("html_url", "")
+    )
+
+    if commit:
+        commit_url = f"{repository_url}/commit/{commit}"
+        commit_message = commit
+    else:
+        head_commit = workflow_info.get("head_commit", {})
+        commit_url = f'{repository_url}/commit/{head_commit.get("id", "")}'
+        commit_message: str = head_commit["message"].split("\n", 1)[0]
+
+    highlight_str = ""
+    if highlight and status is not Status.SUCCESS:
+        highlight_str = "".join([f"@{h}\n" for h in highlight])
 
     return template.format(
-        status=status,
-        workflow=workflow,
-        repository=repository,
-        branch=branch,
-        commit=commit,
-        highlight=highlight,
+        status=workflow_status.value,
+        workflow=linker(workflow_name, workflow_url),
+        repository=linker(repository, repository_url),
+        branch=linker(branch, branch_url),
+        commit=linker(commit_message, commit_url),
+        highlight=highlight_str,
     )
 
 
