9d32baa267e385b6a4d167020af8a4b61f162f70
Björn Ricks <bjoern.ricks@greenbone.net>
2023-07-17 16:34:12 +0200

    Change: Refactor mattermost_notify
    * Allow to pass an explicit commit
    * Split into several modules
    * Add more unit tests

--- tests/test_git.py
+++ tests/test_git.py
@@ -1,68 +1,127 @@
-# Copyright (C) 2022 Jaspar Stach <jasp.stac@gmx.de>
+# SPDX-FileCopyrightText: 2022-2023 Greenbone AG
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# ruff: noqa: E501
 
 import unittest
 from unittest.mock import MagicMock, patch
 
-from mattermost_notify.git import _linker as linker
-from mattermost_notify.git import fill_template, parse_args
+from mattermost_notify.git import fill_template, linker
+from mattermost_notify.status import Status
 
 
-class GitNotifyTestCase(unittest.TestCase):
+class LinkerTestCase(unittest.TestCase):
     def test_linker(self):
-        expected = "[foo](www.foo.com)"
-        link = linker("foo", "www.foo.com")
+        self.assertEqual(linker("foo", "www.foo.com"), "[foo](www.foo.com)")
 
-        self.assertEqual(link, expected)
+    def test_no_url(self):
+        self.assertEqual(linker("foo"), "foo")
+        self.assertEqual(linker("foo", None), "foo")
+        self.assertEqual(linker("foo", ""), "foo")
 
-    def test_argument_parsing(self):
-        parsed_args = parse_args(["www.url.de", "channel"])
 
-        self.assertEqual(parsed_args.url, "www.url.de")
-        self.assertEqual(parsed_args.channel, "channel")
+class FillTemplateTestCase(unittest.TestCase):
+    def test_success_no_highlight(self):
+        actual = fill_template(
+            highlight=["user1", "user2"],
+            status=Status.SUCCESS.name,
+            terminal=MagicMock(),
+        )
+        expected = """#### Status: :white_check_mark: success
 
-    def test_fail_argument_parsing(self):
-        with self.assertRaises(SystemExit):
-            parse_args(["-s"])
+| Workflow |  |
+| --- | --- |
+| Repository (branch) |  ([](/tree/None)) |
+| Related commit | [](/commit/) |
+
+"""
+        self.assertEqual(expected, actual)
+
+    def test_failure_highlight(self):
+        actual = fill_template(
+            highlight=["user1", "user2"],
+            status=Status.FAILURE.name,
+            terminal=MagicMock(),
+        )
+        expected = """#### Status: :x: failure
+
+| Workflow |  |
+| --- | --- |
+| Repository (branch) |  ([](/tree/None)) |
+| Related commit | [](/commit/) |
+
+@user1
+@user2
+"""
+        self.assertEqual(expected, actual)
+
+    def test_short_template(self):
+        actual = fill_template(
+            short=True,
+            status=Status.SUCCESS.name,
+            workflow_name="SomeWorkflow",
+            workflow_id="w1",
+            commit="12345",
+            repository="foo/bar",
+            branch="main",
+            terminal=MagicMock(),
+        )
+        expected = (
+            ":white_check_mark: success: [SomeWorkflow](https://github.com/foo/bar/actions/runs/w1) "
+            "([12345](https://github.com/foo/bar/commit/12345)) in "
+            "[foo/bar](https://github.com/foo/bar) ([main](https://github.com/foo/bar/tree/main))"
+        )
+        self.assertEqual(expected, actual)
+
+    def test_template(self):
+        actual = fill_template(
+            short=False,
+            status=Status.SUCCESS.name,
+            workflow_name="SomeWorkflow",
+            workflow_id="w1",
+            commit="12345",
+            repository="foo/bar",
+            branch="main",
+            terminal=MagicMock(),
+        )
+        expected = """#### Status: :white_check_mark: success
+
+| Workflow | [SomeWorkflow](https://github.com/foo/bar/actions/runs/w1) |
+| --- | --- |
+| Repository (branch) | [foo/bar](https://github.com/foo/bar) ([main](https://github.com/foo/bar/tree/main)) |
+| Related commit | [12345](https://github.com/foo/bar/commit/12345) |
+
+"""
+        self.assertEqual(expected, actual)
 
     @patch("mattermost_notify.git.get_github_event_json")
-    def test_no_highlight(self, event_mock: None):
-        event_mock.return_value = None
-        parsed_args = parse_args(
-            ["www.url.de", "channel", "--highlight", "user1", "user2"]
-        )
-        rf = fill_template(parsed_args, MagicMock())
-        rt = (
-            "#### Status: :white_check_mark: success\n\n"
-            "| Workflow | [None](https://githu"
-            "b.com/None/actions/runs/None) |\n| --- | --- |"
-            "\n| Repository (branch) | [None](https://githu"
-            "b.com/None) (None) |\n| Related commit | not a"
-            "vailable |\n\n"
-        )
-        print(rf)
-        self.assertEqual(rf, rt)
+    def test_template_data_from_github_event(self, mock: MagicMock):
+        event = {
+            "workflow_run": {
+                "conclusion": Status.SUCCESS.name,
+                "name": "SomeWorkflow",
+                "head_repository": {
+                    "full_name": "foo/bar",
+                    "html_url": "https://github.com/foo/bar",
+                },
+                "head_branch": "main",
+                "head_commit": "12345",
+                "workflow_id": "w1",
+            }
+        }
+        mock.return_value = event
 
-    @patch("mattermost_notify.git.get_github_event_json")
-    def test_highlight(self, event_mock: None):
-        event_mock.return_value = None
-        parsed_args = parse_args(
-            [
-                "www.url.de",
-                "channel",
-                "--highlight",
-                "user1",
-                "user2",
-                "-S",
-                "failure",
-            ]
+        actual = fill_template(
+            short=False,
+            terminal=MagicMock(),
         )
-        rf = fill_template(parsed_args, MagicMock())
-        rt = (
-            "#### Status: :x: failure\n\n"
-            "| Workflow | [None](https://githu"
-            "b.com/None/actions/runs/None) |\n| --- | --- |"
-            "\n| Repository (branch) | [None](https://githu"
-            "b.com/None) (None) |\n| Related commit | not a"
-            "vailable |\n\n@user1\n@user2\n"
-        )
-        self.assertEqual(rf, rt)
+        expected = """#### Status: :white_check_mark: success
+
+| Workflow | [SomeWorkflow](https://github.com/foo/bar/actions/runs/w1) |
+| --- | --- |
+| Repository (branch) | [foo/bar](https://github.com/foo/bar) ([main](https://github.com/foo/bar/tree/main)) |
+| Related commit | [12345](https://github.com/foo/bar/commit/12345) |
+
+"""
+        self.assertEqual(expected, actual)
