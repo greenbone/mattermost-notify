8080a9e03c0e2a4a2b68378f1f8139c5f76399ed
Paul Scheunemann <paul.scheunemann@greenbone.net>
2024-11-18 15:48:27 +0100

    Change: Allow for colored messages
    Ref: DEVOPS-1269

--- mattermost_notify/post.py
+++ mattermost_notify/post.py
@@ -2,14 +2,51 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from enum import Enum
 import httpx
 from pontos.typing import SupportsStr
 
 from mattermost_notify.errors import MattermostNotifyError
 
+class Colors(Enum):
+    """
+    Colors namespace for mattermost notifications.
+    The colors are borrowed from bootstrap 5.
+    """
 
-def post(url: str, channel: str, text: SupportsStr) -> None:
-    response = httpx.post(url=url, json={"channel": channel, "text": text})
+    SECONDARY = "#6c757d"
+    SUCCESS = "#28a745"
+    WARNING = "#e0a800"
+    DANGER = "#c82333"
+
+def post(
+    url: str,
+    channel: str,
+    text: SupportsStr,
+    color: str = Colors.SECONDARY
+) -> None:
+    """
+    Post a message to a Mattermost channel.
+
+    Args:
+        url (str): The Mattermost webhook URL.
+        channel (str): The channel name to post the message to.
+        text (SupportsStr): The message content, markdown formatted.
+        color (str, optional): The color of the message, visible as a
+                               border on the left. Defaults to Colors.SECONDARY.
+
+    Raises:
+        MattermostNotifyError: If the HTTP request fails.
+    """
+    
+    response = httpx.post(url=url, json={"channel": channel, "attachments": [
+            {
+                "color": color,
+                "text": text,
+                "fallback": text,
+            }
+        ]})
+
     if not response.is_success:
         raise MattermostNotifyError(
             "Failed to post on Mattermost. HTTP status was "
